57419bad6b56a6ef7564dd3b4da3b2d2422b9586
liquidz <liquidz.uo@gmail.com>
2016-12-06 13:54:38 +0000

    wip

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+extern crate url;
+
+use std::env;
+// use url::percent_encoding::lossy_utf8_percent_decode;
+use url::percent_encoding::percent_decode;
+
+
+fn main() {
+    // let key = "QUERY_STRING";
+    match env::var("HOME") {
+        Ok(val) => println!("home = {}", val),
+        Err(e) => println!("err: {}", e),
+    }
+    let sample = "g+http%3A%2F%2Fwww.nifty.com%3Ffoo%3Dbar";
+    println!("sample: {}", sample);
+
+    match percent_decode(sample.as_bytes()).decode_utf8() {
+        Ok(res) => println!("res: {}", res),
+        Err(e) => println!("err: {}", e),
+    }
+}
