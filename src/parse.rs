9c27628e74ae9aa0cedb1e0f0c312928d3db2ba6
liquidz <liquidz.uo@gmail.com>
2016-12-12 04:44:32 -0500

    update

--- src/parse.rs
+++ src/parse.rs
@@ -1,30 +1,24 @@
 use std::str::FromStr;
-// use Error;
+use std::io::{Error, ErrorKind};
 
 #[derive(Debug)]
 enum Request {
     Url(String),
     Search(String, String),
-    InvalidRequest,
 }
 
-// impl FromStr for Request {
-//    fn split(s: &str) -> Vec<&str> {
-//        let res: Vec<&str> = s.splitn(2, ' ').collect();
-//        res
-//    }
-//
-//    fn from_str(s: &str) -> Result<Request, Error> {
-//        let res = split(s);
-//        match res.len() {
-//            1 => Ok(Request::Url(res[0].to_string())),
-//            2 => Ok(Request::Search(res[0].to_string(), res[1].to_string())),
-//            Error::
-//            _ => Err(Error::)
-//                Request::InvalidRequest,
-//        }
-//    }
-// }
+impl FromStr for Request {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Request, Error> {
+        let res: Vec<&str> = s.splitn(2, ' ').collect();
+        match res.len() {
+            1 => Ok(Request::Url(res[0].to_string())),
+            2 => Ok(Request::Search(res[0].to_string(), res[1].to_string())),
+            _ => Err(Error::new(ErrorKind::InvalidInput, "invalid")),
+        }
+    }
+}
 
 // fn split(s: &str) -> Vec<&str> {
 //    let res: Vec<&str> = s.splitn(2, ' ').collect();
